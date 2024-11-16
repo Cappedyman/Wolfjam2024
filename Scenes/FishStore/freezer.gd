0cb7ae35cc821c5508e9ea859aeba4a07c30c3fa
Caleb Clapper <104874097+Caleb518c@users.noreply.github.com>
2024-11-16 13:41:32 -0500

    fish 1
    fish 1

--- Scenes/FishStore/freezer.gd
+++ Scenes/FishStore/freezer.gd
@@ -42,6 +42,6 @@ func _on_freezer_door_reverse_body_exited(body: Node2D) -> void:
 
 
 func _on_fish_body_entered(body: Node2D) -> void:
-	StaticInventory.add_item("77")
+	StaticInventory.add_item("1")
 	$Fish.queue_free()
 	print(StaticInventory.getInv())
