6e5483e3228c237f271480f59e4f81b327c7e5ba
Caleb Clapper <104874097+Caleb518c@users.noreply.github.com>
2024-11-16 00:00:41 -0500

    added fish shop and camera to cat
    added fish shop and camera to cat

--- Scenes/FishStore/fish_store.gd
+++ Scenes/FishStore/fish_store.gd
@@ -2,7 +2,6 @@ extends Node2D
 
 var canEnterFishDoor: bool = false
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,14 +10,14 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if canEnterFishDoor and Input.is_action_just_pressed("Interact"):
-		get_tree().change_scene_to_file("res://Scenes/FishStore/FishStore.tscn");
+		get_tree().change_scene_to_file("res://Scenes/Street/Street.tscn");
 
 
-func _on_fish_door_body_entered(body: Node2D) -> void:
+func _on_fish_door_reverse_body_entered(body: Node2D) -> void:
 	if body.name == "Cat":
-		canEnterFishDoor = true 
-		
+			canEnterFishDoor = true 
 
-func _on_fish_door_body_exited(body: Node2D) -> void:
+
+func _on_fish_door_reverse_body_exited(body: Node2D) -> void:
 	if body.name == "Cat":
 		canEnterFishDoor = false
--- Scenes/Player/cat.gd
+++ Scenes/Player/cat.gd
@@ -1,25 +1,33 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+var speed = 350  
+var acceleration = 800  
+var friction = 3000 
+var jumpVelocity = -400
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	# Handle jump
+	if Input.is_action_pressed("Jump") and is_on_floor():
+		velocity.y = jumpVelocity
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	# Handle movement input
+	if Input.is_action_pressed("RunLeft"):
+		velocity.x -= acceleration * delta
+	elif Input.is_action_pressed("RunRight"):
+		velocity.x += acceleration * delta
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		# Apply friction when no movement keys are pressed
+		if velocity.x > 0 and is_on_floor():
+			velocity.x = max(0, velocity.x - friction * delta)
+		elif velocity.x < 0 and is_on_floor():
+			velocity.x = min(0, velocity.x + friction * delta)
 
+	# Clamp the velocity to the max speed.
+	velocity.x = clamp(velocity.x, -speed, speed)
+
+	# Apply the movement.
 	move_and_slide()
